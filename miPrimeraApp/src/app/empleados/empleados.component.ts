6adadb8034dfe83d1ada97ded330d5ffcfac0f94
daniescobar14 <49284609+daniescobar14@users.noreply.github.com>
2021-05-23 22:53:01 +0200

    video 5

---
+++ miPrimeraApp/src/app/empleados/empleados.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+//creamos el decorador
+@Component({
+    selector: "app-empleados",
+    templateUrl: "./empleados.component.html",
+    styleUrls:['./empleados.component.css']
+
+})
+
+//esto es para llamar a la clase
+export class EmpleadosComponent {
+
+}
