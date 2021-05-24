722e49fa96dc125af78a50e719efd365c89ac276
daniescobar14 <49284609+daniescobar14@users.noreply.github.com>
2021-05-24 22:28:14 +0200

    video 6

--- miPrimeraApp/src/app/app.module.ts
+++ miPrimeraApp/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { EmpleadosComponent } from './empleados/empleados.component';
 import { EmpleadoComponent } from './empleado/empleado.component';
+import { EjemplotirarComponent } from './ejemplotirar/ejemplotirar.component';
 
 @NgModule({
   declarations: [
-    AppComponent, EmpleadosComponent, EmpleadoComponent
+    AppComponent, EmpleadosComponent, EmpleadoComponent, EjemplotirarComponent
   ],
   imports: [
     BrowserModule
--- miPrimeraApp/src/app/empleado/empleado.component.ts
+++ miPrimeraApp/src/app/empleado/empleado.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-empleado',
+  //template:"<p>Aqui va uno de los empleados</p>",
   templateUrl: './empleado.component.html',
   styleUrls: ['./empleado.component.css']
+  //podemos llamar a la clase de estilos externa, o podemos montar el estilo directamente en el archivo ts
+  //styles:["p{background-color:red;}"]
 })
 export class EmpleadoComponent implements OnInit {
 
