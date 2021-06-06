bf03be57f7fd3325be3399fa3ccd1df854eee635
daniescobar14 <49284609+daniescobar14@users.noreply.github.com>
2021-06-06 12:46:12 +0200

    cambio a macbook air

--- miPrimeraApp/src/app/empleado/empleado.component.ts
+++ miPrimeraApp/src/app/empleado/empleado.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './empleado.component.html',
   styleUrls: ['./empleado.component.css']
   //podemos llamar a la clase de estilos externa, o podemos montar el estilo directamente en el archivo ts
-  //styles:["p{background-color:red;}"]
+  //styles:["p{background-color:red;}"] //a esto se llama colocar componentes inline
 })
 export class EmpleadoComponent implements OnInit {
 
